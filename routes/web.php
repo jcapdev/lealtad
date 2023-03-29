ae8c55341fcda844562b0141292ca1700b6bdaa0
jcapdev <aguila08ruso2@gmail.com>
2023-03-29 10:39:36 -0600

    cambios en sessiones , rutas  crud de prueba

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 use  App\Http\Controllers\Admin\{AuthContoller,ProfileController,UserController};
+use  App\Http\Controllers\{CustomerController};
 
 
 Route::get('/', function () {
@@ -23,13 +24,23 @@ Route::get('/admin/login',[AuthContoller::class,'getLogin'])->name('getLogin');
 
 Route::post('/admin/login',[AuthContoller::class,'postLogin'])->name('postLogin');
 
+
+
 Route::group(['middleware'=>['admin_auth']],function(){
-    
+    Route::resource('/admin/customers', CustomerController::class);
+
     Route::get('/admin/dashboard',[ProfileController::class,'dashboard'])->name('dashboard');
 
     Route::get('/admin/users',[UserController::class,'index'])->name('users.index');
 
     Route::get('/admin/logout',[ProfileController::class,'logout'])->name('logout');
+/*
+    Route::get('/admin/customer', [CustomerController::class,'index'])->name('index');
+    Route::get('/admin/customer/create', [CustomerController::class,'create'])->name('create');
+    Route::get('/admin/customer/destroy', [CustomerController::class,'destroy'])->name('destroy');
+    Route::get('/admin/customer/show', [CustomerController::class,'show'])->name('show');
+    Route::get('/admin/customer/edit', [CustomerController::class,'edit'])->name('edit');*/
 
+    
 });
 
